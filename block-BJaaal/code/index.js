26dc9b69fa68e10cb8a53eaaf7a74b8c9fe745a4
SeemaDSouza827 <seemadsouza827@gmail.com>
2022-03-14 21:56:22 +0530

    My way through Java Script with escape characters

--- block-BJaaal/code/index.js
+++ block-BJaaal/code/index.js
@@ -34,3 +34,11 @@ const taxPercent = 10;
   `
   - Above message should be in two lines
 */
+const userName="Seema";
+let bio =`Learning to code.`;
+alert(`I am`+ " " + userName +`and my bio is`+" " + bio);
+alert(`I'm` +" "+ firstName+ " " + lastName +" "+` of Winterfell`);
+alert(`I am ${userName} and my bio is ${bio}`);
+alert(`I'm ${firstName} ${lastName} of Winterfell`);
+alert(`The total amount is ${amount} \n the tax amount is ${(amount* taxPercent)/100}`);
+alert("The total amount is " + amount + "\n the tax amount is " + (amount* taxPercent)/100);
